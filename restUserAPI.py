ff093157b68fee9ea8c9353abee8ff1b654d8280
Jessica Martinez Marquez <jessmtzm@bu.edu>
2022-04-23 19:37:23 -0400

    post updates

--- restUserAPI.py
+++ restUserAPI.py
@@ -1,3 +1,5 @@
+from typing import Dict
+from attr import attr
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from users import create_user, find_user
@@ -9,16 +11,23 @@ api = Api(app)
 
 class restWeb(Resource):
     def get(self):
+        ''' 
+        Fetches user information from the database
+        '''
+
         return find_user("jessica")
         # return create_user("userInfo_1290381.json", "903810847"), 200
 
     def post(self):
+        '''
+        Creates a new user in the database based on the provided information
+        '''
         parser = reqparse.RequestParser()
         parser.add_argument("f_name", required=True, type=str, location='form')
         parser.add_argument("l_name", required=True, type=str, location='form')
         parser.add_argument('role', required=True, type=str, location='form')
         # parser.add_argument('attributes', required=True, type=str, location='form')
-        parser.add_argument('patient', required=False, type=str, location='form', action='append')
+        parser.add_argument('patients', required=False, type=str, location='form', action='append')
         parser.add_argument('DOB', required=False, type=str, location='form')
         parser.add_argument('assigned_doctor', required=False, type=str, location='form')
 
@@ -29,16 +38,36 @@ class restWeb(Resource):
                 "last_name": args['l_name']
             },
             "role": args['role'],
-            "attributes": {
-                "patients": args['patient']
-            }
+        }
+
+        attributes = {}
+
+        try:
+            if args['role'] == 'doctor':
+                attributes = {
+                    # "patients": args['patients']
+                    "patients": []
+                }
+
+            elif args['role'] == 'patient': 
+
+                attributes = {
+                    "DOB": args['DOB'],
+                    "assigned_doctor": args['assigned_doctor'],
+                }
+        except:
+            print("No attributes")
+        
+        userInfo['attributes'] = attributes
+        print("\nUSER INFO")
+        print(userInfo)
             # 'Name': args['name'],
             # 'Type': args['type'],
             # 'Gender': args['gender'],
             # 'AccName': args['accName'],
             # 'Password': args['password']
-        }
-        response = create_user(userInfo, args)
+        
+        response = create_user(userInfo)
         if isinstance(response,list):
         #userinfo = json.dumps(userInfo, indent=4)
         #print(userinfo)
@@ -49,8 +78,9 @@ class restWeb(Resource):
             return response, 200
 
 #curl -X POST http://127.0.0.1:5000/users?name=rhett&type=doctor&gender=male&accName=rt123&password=123456
-# CORRECT: curl -d 'name=rhett' -d 'type=doctrs' -d 'gender=male' -d 'accName=12322v' -d 'password=12121' http://127.0.0.1:5000/users 
-# curl -d 'f_name=rhett' -d 'l_name=terrier' -d 'role=doctor' -d 'DOB=01/01/2000' -d 'assigned_doctor=dr.juana' -d 'password=12121' http://127.0.0.1:5000/users
+# curl -d 'name=rhett' -d 'type=doctrs' -d 'gender=male' -d 'accName=12322v' -d 'password=12121' http://127.0.0.1:5000/users 
+# CORRECT: curl -d 'f_name=rhett' -d 'l_name=terrier' -d 'role=doctor' -d 'DOB=01/01/2000' -d 'assigned_doctor=dr.juana' -d 'password=12121' http://127.0.0.1:5000/users
+# CORRECT: curl -d 'f_name=rhett' -d 'l_name=terrier' -d 'role=patient' -d 'DOB=01/01/2000' -d 'assigned_doctor=dr.juana' -d 'password=12121' http://127.0.0.1:5000/users
 
 api.add_resource(restWeb, '/users')
 
@@ -63,7 +93,4 @@ To do
 How to request the api from react
 How to identify doctor or patient
 Do I need request parser if I already validate JSON schema?
-
-
-
 '''
