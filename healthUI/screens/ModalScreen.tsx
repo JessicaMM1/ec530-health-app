99388c4c7558469a9b873ce771da15b8b5d7737b
RJLimBU <kevinlim@bu.edu>
2022-04-24 03:54:55 -0400

    add app.yaml and edit login screen

--- healthUI/screens/ModalScreen.tsx
+++ healthUI/screens/ModalScreen.tsx
@@ -1,15 +1,71 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, SafeAreaView, TextInput, Button, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+
 export default function ModalScreen() {
+
+  //const UselessTextInput = () => {
+  const [textAcc, onChangeAcc] = React.useState(null);
+  const [textPw, onChangePw] = React.useState(null);
+
+  const login = ()=>{
+    fetch('https://example.com/data').then((response) => response.json()).then((json) => {
+      return data.names;
+    }).catch((error) => {
+      console.error(error);
+    });
+    }
+
+  const register = ()=>{
+      setReload(true)
+      var len = Object.keys(items).length
+      if(len>0){
+        loadList()
+      }
+    }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+      <View style={styles.titlePosition}>
+        <Text style={styles.title}>Log in</Text>
+      </View>
+
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/ModalScreen.tsx" />
+
+      
+    <SafeAreaView style={styles.safeAreaPos}>
+      <TextInput 
+        style={styles.input} 
+        onChangeText={onChangeAcc} 
+        value={textAcc} 
+        placeholder="username"
+        keyboardType="default"
+      />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangePw}
+        value={textPw}
+        placeholder="password"
+        keyboardType="default"
+      />
+    </SafeAreaView>
+
+    <TouchableOpacity style={styles.loginbutton} onPress={login}>
+
+      <Text style={styles.loginText}>LOGIN</Text>
+
+    </TouchableOpacity>
+
+    <TouchableOpacity style={styles.regbutton} onPress={register}>
+
+      <Text style={styles.regText}>REGISTER</Text>
+
+    </TouchableOpacity>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
@@ -21,15 +77,58 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
+  titlePosition: {
+    alignItems: 'center',
+    marginTop: 40,
+  },
   separator: {
-    marginVertical: 30,
+    marginVertical: 20,
     height: 1,
     width: '80%',
+    alignItems: 'center',
+    marginTop: 20,
   },
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+
+  safeAreaPos: {
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  loginbutton: {
+    alignItems: 'center',
+    width: '40%',
+    backgroundColor: 'lightskyblue',
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 15
+  },
+
+  loginText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  regbutton: {
+    alignItems: 'center',
+    width: '40%',
+    backgroundColor: 'lightskyblue',
+    padding: 10,
+    marginTop: 5,
+    borderRadius: 15
+  },
+  regText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
 });
