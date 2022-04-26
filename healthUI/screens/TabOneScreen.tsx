572ee1de4da1c7b81e3cce366c92124822da97aa
Jessica Martinez Marquez <jessmtzm@bu.edu>
2022-04-25 22:48:48 -0400

    basic profile screen

--- healthUI/screens/TabOneScreen.tsx
+++ healthUI/screens/TabOneScreen.tsx
@@ -1,5 +1,6 @@
 // User profile page
 
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
@@ -7,18 +8,23 @@ import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>User Profile</Text>
+      <Text style={styles.title}>Welcome</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
 
-      {/* <View>
-        <Text>Welcome {username}</Text>
-        <Text>{f_name}</Text>
+      <View>
+        <Text style={styles.text}>Name Placeholder</Text>
+        <Text style={styles.text}>Role placeholder</Text>
+        <Text style={styles.text}>DOB placeholder</Text>
+        {/* <Text>{f_name}</Text>
         <Text>{l_name}</Text>
         <Text>{role}</Text>
-        <Text>{dob}</Text>
-      </View> */}
+        <Text>{dob}</Text> */}
+      </View>
+
+
     </View>
   );
 }
@@ -27,15 +33,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
   title: {
-    fontSize: 20,
+    fontSize: 40,
     fontWeight: 'bold',
+    marginTop: 40
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
   },
+  text: {
+    fontSize: 20,
+    marginVertical: 5
+  }
 });
