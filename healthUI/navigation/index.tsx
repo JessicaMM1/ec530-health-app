57441ad8864b0d4b5a056182bbeb82fc6cbbe403
Jessica Martinez Marquez <jessmtzm@bu.edu>
2022-04-26 00:12:28 -0400

    chat screen

--- healthUI/navigation/index.tsx
+++ healthUI/navigation/index.tsx
@@ -90,7 +90,7 @@ function BottomTabNavigator() {
         name="TabTwo"
         component={TabTwoScreen}
         options={{
-          title: 'Tab Two',
+          title: 'Chat',
           tabBarIcon: ({ color }) => <TabBarIcon name="comment" color={color} />,
         }}
       />
--- healthUI/screens/TabTwoScreen.tsx
+++ healthUI/screens/TabTwoScreen.tsx
@@ -6,9 +6,8 @@ import { Text, View } from '../components/Themed';
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>health app screen two</Text>
+      <Text style={styles.title}>Chat</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
     </View>
   );
 }
